beb3b52dfcbf816b92ab6a98e8a7f58df1547912
Poiro-cat <xhn15@tsinghua.org.cn>
2024-10-01 00:50:49 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from primality_test.polynomial import Polynomial
-from primality_test.elliptic_curve import Elliptic_curve
-from primality_test.algorithms import (Trial_division,Trial_division_30,Trial_division_lim,
+from .primality_test.polynomial import Polynomial
+from .primality_test.elliptic_curve import Elliptic_curve
+from .primality_test.algorithms import (Trial_division,Trial_division_30,Trial_division_lim,
                                        Fermat,Miller_Rabin,Solovay_Strassen,
                                        Lucas_prob_prime,Strong_Lucas_prob_prime,Extra_strong_Lucas_prob_prime,Baillie_PSW,
                                        Lucas,Pocklington,Proth,Pepin,Factorizing_Lucas_prob_prime_v1,Factorizing_Lucas_prob_prime_v2,Lucas_Lehmer,Lucas_Lehmer_Riesel,
