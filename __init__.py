71332222edc9e73fd0fbaea419bfafa2f4b00867
Poiro-cat <xhn15@tsinghua.org.cn>
2024-10-08 16:57:02 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from .primality_test.polynomial import Polynomial
 from .primality_test.elliptic_curve import Elliptic_curve
-from .primality_test.algorithms import (Trial_division,Trial_division_30,Trial_division_lim,
-                                       Fermat,Miller_Rabin,Solovay_Strassen,
-                                       Lucas_prob_prime,Strong_Lucas_prob_prime,Extra_strong_Lucas_prob_prime,Baillie_PSW,
-                                       Lucas,Pocklington,Proth,Pepin,Factorizing_Lucas_prob_prime_v1,Factorizing_Lucas_prob_prime_v2,Lucas_Lehmer,Lucas_Lehmer_Riesel,
-                                       Goldwasser_Kilian,Atkin_Morain,Tsumura,
-                                       Quartic_field,Cubic_Sextic_field,APR_CL,
-                                       Frobenius_prob_prime,Strong_Frobenius_prob_prime,Quadratic_Frobenius,Random_Quadratic_Frobenius,AKS
+from .primality_test.algorithms import (Trial_division, Trial_division_30, Trial_division_lim,
+                                       Fermat, Miller_Rabin, Solovay_Strassen,
+                                       Lucas_prob_prime, Strong_Lucas_prob_prime, Extra_strong_Lucas_prob_prime, Baillie_PSW,
+                                       Lucas, Pocklington, Proth,Pepin,Factorizing_Lucas_prob_prime_v1, Factorizing_Lucas_prob_prime_v2, Lucas_Lehmer, Lucas_Lehmer_Riesel,
+                                       Goldwasser_Kilian, Atkin_Morain, Tsumura,
+                                       Cubic_field, Quartic_field,Sextic_field, APR_CL,
+                                       Frobenius_prob_prime, Strong_Frobenius_prob_prime, Quadratic_Frobenius, Random_Quadratic_Frobenius, AKS
                                        )
